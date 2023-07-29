fae43268fdfb572908d20843b74d601461e42d11
Janne Wernecken <janne.wernecken@study.hs-duesseldorf.de>
2023-07-29 22:17:44 +0200

    preprocessing: add encoding for demographic data, fix column naming in one-hot-encoding function

--- student-matcher/local_helpers.py
+++ student-matcher/local_helpers.py
@@ -2,63 +2,75 @@ import re
 import pandas as pd
 
 
-def add_column_to_data_and_short_labels(data, columns, new_short_label, column_data, full_question):
-    short_labels = columns.index.tolist()
-    data[full_question] = column_data
+def add_column_to_data_and_short_labels(df, all_columns, new_short_label, column_df, full_question):
+    short_labels = all_columns.index.tolist()
+
+    # add the new column to the dataframe
+    df[full_question] = column_df
+
+    # add the new column to the short labels
     short_labels.append(new_short_label)
-    short_labels_and_full_questions = dict(zip(short_labels, data.columns))
-    columns = pd.Series(short_labels_and_full_questions)
-    columns[new_short_label] = full_question
+    short_labels_and_full_questions = dict(zip(short_labels, df.columns))
+    all_columns = pd.Series(short_labels_and_full_questions)
 
-    return data, columns
+    # update the value of the new column to be the full question
+    all_columns[new_short_label] = full_question
 
-def remove_column_from_data_and_short_labels(data, columns, column_to_drop_short_label):
-    # columns_to_drop = [columns.lieblingsmusiker]
-    # data = data.drop(columns_to_drop, axis=1)
-    
-    data = data.drop([columns[column_to_drop_short_label]], axis=1)
+    return df, all_columns
 
-    short_labels = columns.index.tolist()
+
+def remove_column_from_data_and_short_labels(df, all_columns, column_to_drop_short_label):
+    # remove the column from the dataframe
+    df = df.drop([all_columns[column_to_drop_short_label]], axis=1)
+
+    # remove the column from the short labels
+    short_labels = all_columns.index.tolist()
     short_labels.remove(column_to_drop_short_label)
+    short_labels_and_full_questions = dict(zip(short_labels, df.columns))
+    all_columns = pd.Series(short_labels_and_full_questions)
 
-    short_labels_and_full_questions = dict(zip(short_labels, data.columns))
-    columns = pd.Series(short_labels_and_full_questions)
-
-    return data, columns
+    return df, all_columns
 
 
 def generate_full_question_from_short_label(short_label):
-    # replace the letters in front of the first _ with the same string but capitalized and with space instead of _
+    # replace the letters in front of the first _ with the same string but capitalized and with space instead of _ and add (encoded)
     full_question = re.sub(r"^[a-zA-Z]+_", lambda match: match.group(0), short_label)
-
     full_question = full_question.replace("_", " ").title() + "? (encoded)"
 
     return full_question
 
 
-def one_hot_encode_columns_and_update_short_labels(data, columns, columns_to_encode_short_labels: list):
+def one_hot_encode_columns_and_update_short_labels(df, columns, columns_to_encode_short_labels: list):
+    # get the full column labels (full questions) of the columns to encode
     columns_to_encode = [columns[column_label] for column_label in columns_to_encode_short_labels]
 
+    # strip, lowercase, replace / with _, replace whitespace with _
     for column in columns_to_encode:
-        data[column] = data[column].str.replace("/", "_").replace("_", "_").str.lower()
+        df[column] = df[column].str.strip().str.lower().replace("/", "_", regex=True).replace("\s", "_", regex=True)
 
-    data_encoded = pd.get_dummies(data, columns=columns_to_encode, prefix=columns_to_encode_short_labels)
+    # one-hot encode the columns
+    df_encoded = pd.get_dummies(df, columns=columns_to_encode, prefix=columns_to_encode_short_labels)
     
+    # find the newly added column labels
     short_labels = columns.index.tolist()
-    new_column_labels = [item for item in list(data_encoded.columns.values) if item.startswith(tuple(column for column in columns_to_encode_short_labels))]
+    new_column_labels = [item for item in list(df_encoded.columns.values) if item.startswith(tuple(column for column in columns_to_encode_short_labels))]
 
+    # add the new column labels to the short labels
     short_labels_encoded = short_labels + list(new_column_labels)
 
+    # remove the columns that were encoded from the short labels
     for column in columns_to_encode_short_labels:
         short_labels_encoded.remove(column)
 
-    print(short_labels_encoded)
-
-    short_labels_and_full_questions_encoded = dict(zip(short_labels_encoded, data_encoded.columns))
+    # update column labels and generate full questions for the new columns
+    short_labels_and_full_questions_encoded = dict(zip(short_labels_encoded, df_encoded.columns))
     short_labels_and_full_questions_encoded = {key: generate_full_question_from_short_label(value) if key in new_column_labels else value for key, value in short_labels_and_full_questions_encoded.items()}
     columns_encoded = pd.Series(short_labels_and_full_questions_encoded)
 
-    return data_encoded, columns_encoded
+    # rename columns in dataframe to full questions
+    df_encoded.rename(short_labels_and_full_questions_encoded, inplace=True, axis=1)
+
+    return df_encoded, columns_encoded
 
 
 def insert_linebreaks(title):
